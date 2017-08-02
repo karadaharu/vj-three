b0dae846d1c9ebcd37ab6d8436eae2577dc2b795
Kazumasa <catter121@gmail.com>
2017-08-02 14:46:55 +0900

    Add nlp server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+var kuromoji = require('kuromoji');
+
+// この builder が辞書やら何やらをみて、形態素解析機を造ってくれるオブジェクトです。
+var builder = kuromoji.builder({
+  // ここで辞書があるパスを指定します。今回は kuromoji.js 標準の辞書があるディレクトリを指定
+  dicPath: 'node_modules/kuromoji/dict'
+});
+
+// 形態素解析機を作るメソッド
+builder.build(function(err, tokenizer) {
+  // 辞書がなかったりするとここでエラーになります(´・ω・｀)
+  if(err) { throw err; }
+
+  // tokenizer.tokenize に文字列を渡すと、その文を形態素解析してくれます。
+  var tokens = tokenizer.tokenize("今日は森へ行った");
+  for ( var i = 0, len = Object.keys(tokens).length; i < len; i++) {
+    console.log(tokens[i]["surface_form"]);
+  }
+});
